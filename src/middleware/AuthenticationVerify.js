5e803a587df9fb2efedabcf97beae589fb109cd8
hasan-sagar <khsagar0512@gmail.com>
2023-09-27 16:46:55 +0600

    first commit

---
+++ src/middleware/AuthenticationVerify.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  let Token = req.headers["token"];
+  jwt.verify(Token, process.env.JWT_SECRET_KEY, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      res.status(401).json({ status: "unauthorized" });
+    } else {
+      let email = decoded["data"];
+      req.headers.email = email;
+      next();
+    }
+  });
+};
